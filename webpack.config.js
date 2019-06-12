1ff5b92eac33f9966e03f0a89f7ccf07b83b8e30
Scott Elder <42284090+scottelder@users.noreply.github.com>
2019-06-11 20:31:06 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const SRC_DIR = path.join(__dirname, '/client');
 const DIST_DIR = path.join(__dirname, '/dist');
 module.exports = {
-  entry: `${SRC_DIR}/app.js`,
+  entry: `${SRC_DIR}/index.js`,
   output: {
   filename: 'bundle.js',
   path: DIST_DIR
